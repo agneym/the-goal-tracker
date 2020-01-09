1cc97a2d4ff0aea7fe9031b8df8cd8a5d93b4eaa
Agney <agney@outlook.in>
2020-01-09 16:29:21 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  useEffect(async () => {
-    const response = await fetch(
-    `/.netlify/functions/hello`
-    );
-    const jsonResponse = await response.json();
-    console.log(jsonResponse);
-  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/">
+        <Dashboard />
+      </Route>
+    </Router>
   );
 }
 
