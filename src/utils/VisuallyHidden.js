15a3be96a64c81958f9111f894492c45e953086d
Agney <agney@outlook.in>
2020-01-10 23:43:05 +0530

    visually hidden utility

---
+++ src/utils/VisuallyHidden.js
@@ -0,0 +1,15 @@
+import { css } from "styled-components";
+
+const VisuallyHidden = css`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  clip-path: inset(50%);
+  border: 0;
+`;
+
+export default VisuallyHidden;
