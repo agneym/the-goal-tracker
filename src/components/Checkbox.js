4185d2cdd178f935a1a07102511dc9f95c044ba2
Agney <agney@outlook.in>
2020-01-10 20:32:29 +0530

    change to htmlFor

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -95,7 +95,7 @@ function Checkbox({ id, labelText, ...rest }) {
   return (
     <Field>
       <Input type="checkbox" id={id} {...rest} />
-      <Label for={id}>
+      <Label htmlFor={id}>
         <TickContainer>
           <svg viewBox="0 0 12 10">
             <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
