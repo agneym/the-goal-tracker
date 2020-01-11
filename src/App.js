92ef0008a76ea73c39a51ce6dffa65550780d9e2
Agney <agney@outlook.in>
2020-01-11 13:22:48 +0530

    add apollo client

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 import Dashboard from "./pages/Dashboard";
 import GlobalStyles from "./utils/Global";
 import theme from "./utils/theme";
+import client from "./client";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </Router>
+      </ApolloProvider>
       <GlobalStyles />
     </ThemeProvider>
   );
---
+++ src/client.js
@@ -0,0 +1,27 @@
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+import { GRAPHQL_ENDPOINT } from "./utils/constants";
+
+const httpLink = createHttpLink({
+  uri: GRAPHQL_ENDPOINT,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
